a461418fc009d0c4f92a21e44fcc0bf55c7b0ae2
Marcio Almeida <marcioadev@gmail.com>
2022-05-31 09:04:53 -0300

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,13 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.26.0',
+  defaultReleaseBranch: 'main',
+  name: 'lambda-stepfunctions-dynamo',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
